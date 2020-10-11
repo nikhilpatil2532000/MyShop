e52570d1ad268475807a90fddf209aa0996d6371
nikhilpatil2532000 <69834562+nikhilpatil2532000@users.noreply.github.com>
2020-10-11 21:32:39 +0530

    Update urls.py

--- myshop/urls.py
+++ myshop/urls.py
@@ -32,5 +32,4 @@ urlpatterns = [
         {'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns += patterns(”, (r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
